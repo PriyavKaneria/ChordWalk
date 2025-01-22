c69ba185215cbce71906fde592a40c22423ba5c8
PriyavKaneria <priyavkaneria@gmail.com>
2025-01-22 20:33:05 +0530

    Beeter configuration UI and more options

--- app/page.tsx
+++ app/page.tsx
@@ -3,30 +3,44 @@ import React, { useState, useEffect, use } from 'react';
 import { Slider } from "@/components/ui/slider";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
-const ChordMidpoint = () => {
+const ChordWalker = () => {
   const [sides, setSides] = useState(3);
   const [chordLength, setChordLength] = useState(30);
-  const [currentPointIndex, setCurrentPointIndex] = useState(0);
+  const [currentPointIndex, setCurrentPointIndex] = useState(-1);
   const [stepPoints, setStepPoints] = useState([] as { x: number; y: number; }[]);
-  const [play, setPlay] = useState(false);
-  const [showActiveNeighbors, setShowActiveNeighbors] = useState(false);
-  const [showNextPredictedPoint, setShowNextPredictedPoint] = useState(false);
-  const [activeForward, setActiveForward] = useState(false);
+  const [point1, setPoint1] = useState<{ x: number; y: number; }>({ x: 0, y: 0 });
   const [point2, setPoint2] = useState<{ x: number; y: number; }>({ x: 0, y: 0 });
+  const [play, setPlay] = useState(false);
+  const [showEllipse, setShowEllipse] = useState(false);
+  const [ellipseA, setEllipseA] = useState(80);
+  const [ellipseB, setEllipseB] = useState(100);
+  const [showActiveNeighbors, setShowActiveNeighbors] = useState(true);
+  const [showNextPredictedPoint, setShowNextPredictedPoint] = useState(true);
+  const [showMidpoint, setShowMidpoint] = useState(true);
+  const [showTracedMidpointPath, setShowTracedMidpointPath] = useState(true);
+  const [tracedMidpointPath, setTracedMidpointPath] = useState([] as { x: number; y: number; }[]);
+  const [activeForward, setActiveForward] = useState(false);
   const [nextPoint2, setNextPoint2] = useState<{ x: number; y: number; }>({ x: 0, y: 0 });
   const totalSteps = 1000;
   const center = { x: 0, y: 0 };
   const defaultPoint = center;
 
-  const generatePolygon = (sides: number) => {
+  const generatePolygon = (sides: number, showEllipse: boolean, a: number, b: number) => {
     const points = [];
     const radius = 80;
     for (let i = 0; i < sides; i++) {
       const angle = (i * 2 * Math.PI) / sides;
-      points.push({
-        x: radius * Math.cos(angle),
-        y: radius * Math.sin(angle)
-      });
+      if (showEllipse) {
+        points.push({
+          x: a * Math.cos(angle),
+          y: b * Math.sin(angle)
+        });
+      } else {
+        points.push({
+          x: radius * Math.cos(angle),
+          y: radius * Math.sin(angle)
+        });
+      }
     }
     return points;
   };
@@ -77,16 +91,16 @@ const ChordMidpoint = () => {
   };
 
   useEffect(() => {
-    const polygonPoints = generatePolygon(sides);
+    const polygonPoints = generatePolygon(sides, showEllipse, ellipseA, ellipseB);
     const points = [];
     for (let i = 0; i < totalSteps + 200; i++) {
       points.push(getPointOnPolygon(polygonPoints, i / totalSteps));
     }
     setStepPoints(points);
-  }, [sides]);
+  }, [sides, showEllipse, ellipseA, ellipseB]);
 
   const getIndexPoints = (index: number) => {
-    const polygonPoints = generatePolygon(sides);
+    const polygonPoints = generatePolygon(sides, showEllipse, ellipseA, ellipseB);
     const actualLength = (chordLength / 100) * 160;
 
     const point1 = stepPoints[index] || defaultPoint;
@@ -112,8 +126,7 @@ const ChordMidpoint = () => {
   };
 
   useEffect(() => {
-    const { point1, intersections } = getIndexPoints(currentPointIndex);
-    const polygonPoints = generatePolygon(sides);
+    const { intersections } = getIndexPoints(currentPointIndex);
 
     if (intersections.length > 0) {
       const closestIntersection = intersections.reduce((closest, current) => {
@@ -128,14 +141,13 @@ const ChordMidpoint = () => {
     if (nextIntersections.length > 0) {
       setNextPoint2(nextIntersections[0]);
     }
-  }, [currentPointIndex, sides, chordLength, stepPoints, activeForward]);
+  }, [currentPointIndex, sides, chordLength, stepPoints, activeForward, showEllipse, ellipseA, ellipseB]);
 
   const checkValidAndSetCurrentPointIndex = (value: number) => {
-    const nextPoint1 = stepPoints[value] || defaultPoint;
     const nextIntersections = getIndexPoints(value).intersections;
-    const nextPoint2 = nextIntersections[0] || defaultPoint;
+    const _nextPoint2 = nextIntersections[0] || defaultPoint;
 
-    if (Math.abs(nextPoint2.x - point2.x) > 10 || Math.abs(nextPoint2.y - point2.y) > 10) {
+    if (Math.abs(_nextPoint2.x - point2.x) > 10 || Math.abs(_nextPoint2.y - point2.y) > 10) {
       const closestIndex = stepPoints.reduce((acc, curr, index) => {
         const distance = Math.sqrt((curr.x - point2.x) ** 2 + (curr.y - point2.y) ** 2);
         if (distance < acc.distance) {
@@ -145,7 +157,11 @@ const ChordMidpoint = () => {
       }, { index: 0, distance: Infinity });
 
       setPoint2(stepPoints[currentPointIndex]);
-      setCurrentPointIndex(closestIndex.index + 1);
+      if (value < currentPointIndex) {
+        setCurrentPointIndex(closestIndex.index - 1);
+      } else {
+        setCurrentPointIndex(closestIndex.index + 1);
+      }
       console.log("switched to ", !activeForward ? "forward" : "backward");
       setActiveForward(!activeForward);
       return;
@@ -154,8 +170,13 @@ const ChordMidpoint = () => {
     setCurrentPointIndex(value);
   };
 
-  const { point1, intersections } = getIndexPoints(currentPointIndex);
-  const polygonPoints = generatePolygon(sides);
+  const { intersections } = getIndexPoints(currentPointIndex);
+  const polygonPoints = generatePolygon(sides, showEllipse, ellipseA, ellipseB);
+
+  useEffect(() => {
+    const { point1: _point1 } = getIndexPoints(currentPointIndex);
+    setPoint1(_point1);
+  }, [currentPointIndex, sides, showEllipse, ellipseA, ellipseB]);
 
   useEffect(() => {
     let interval: NodeJS.Timeout;
@@ -167,95 +188,161 @@ const ChordMidpoint = () => {
     return () => clearInterval(interval);
   }, [play, currentPointIndex]);
 
+  useEffect(() => {
+    if (point1.x === 0 && point1.y === 0) return;
+    if (point2.x === 0 && point2.y === 0) return;
+    if (point1.x === point2.x && point1.y === point2.y) return;
+    // add midpoint to tracedMidpointPath
+    setTracedMidpointPath([...tracedMidpointPath, { x: (point1.x + point2.x) / 2, y: (point1.y + point2.y) / 2 }]);
+  }, [currentPointIndex]);
+
+  useEffect(() => {
+    setCurrentPointIndex(0);
+    setTimeout(() => {
+      setTracedMidpointPath([]);
+    }, 200);
+  }, [sides, showEllipse, ellipseA, ellipseB]);
+
   return (
-    <Card className="w-full max-w-2xl mx-auto">
+    <Card className="w-full max-w-2xl mx-auto my-3">
       <CardHeader>
-        <CardTitle>Chord Midpoint Path</CardTitle>
+        <CardTitle>Chord Walker</CardTitle>
       </CardHeader>
       <CardContent>
         <div className="space-y-6"></div>
-        <svg viewBox="-100 -100 200 200" className="w-full h-96 bg-slate-50">
-          <path
-            d={`M ${polygonPoints.map(p => `${p.x},${p.y}`).join(' L ')} Z`}
-            fill="none"
-            stroke="black"
-            strokeWidth="1"
-          />
-          <circle cx={center.x} cy={center.y} r="3" fill="black" />
-          {showNextPredictedPoint && (
-            <circle cx={nextPoint2.x} cy={nextPoint2.y} r="4" fill="orange" />
-          )}
-          <line
-            x1={point1.x}
-            y1={point1.y}
-            x2={point2.x}
-            y2={point2.y}
-            stroke="green"
-            strokeWidth="2"
-          />
-          {/* Intersection points */}
-          {showActiveNeighbors && intersections.map((point, index) => (
-            <circle key={index} cx={point.x} cy={point.y} r="3" fill="grey" />
-          ))}
-
-          <circle cx={point2.x} cy={point2.y} r="3" fill="blue" />
-          <circle cx={point1.x} cy={point1.y} r="3" fill="red" />
-
-
-        </svg>
-
-        <div className="space-y-4">
-          <div>
-            <label className="block text-sm font-medium mb-2">Number of Sides: {sides}</label>
-            <Slider
-              value={[sides]}
-              min={3}
-              max={10}
-              step={1}
-              onValueChange={(value) => setSides(value[0])}
+        <div className='w-full h-96 mb-3 bg-slate-50 relative'>
+          <svg viewBox="-100 -100 200 200" className="w-full h-full">
+            <path
+              d={`M ${polygonPoints.map(p => `${p.x},${p.y}`).join(' L ')} Z`}
+              fill="none"
+              stroke="black"
+              strokeWidth="1"
             />
-          </div>
-
-          <div>
-            <label className="block text-sm font-medium mb-2">Step: {currentPointIndex} / 1000</label>
-            <Slider
-              value={[currentPointIndex]}
-              min={0}
-              max={totalSteps - 1 + 200}
-              step={1}
-              onValueChange={(value) => checkValidAndSetCurrentPointIndex(value[0])}
+            <circle cx={center.x} cy={center.y} r="1" fill="black" opacity={0.7} />
+            {showNextPredictedPoint && (
+              <circle cx={nextPoint2.x} cy={nextPoint2.y} r="4" fill="orange" />
+            )}
+            <line
+              x1={point1.x}
+              y1={point1.y}
+              x2={point2.x}
+              y2={point2.y}
+              stroke="green"
+              strokeWidth="2"
             />
+            {/* Intersection points */}
+            {showActiveNeighbors && intersections.map((point, index) => (
+              <circle key={index} cx={point.x} cy={point.y} r="3" fill="grey" />
+            ))}
+
+            {showTracedMidpointPath && (
+              <path
+                d={`M ${tracedMidpointPath.map(p => `${p.x},${p.y}`).join(' L ')}`}
+                fill="none"
+                stroke="orange"
+                strokeWidth="1"
+              />
+            )}
+
+            <circle cx={point2.x} cy={point2.y} r="3" fill="blue" />
+            <circle cx={point1.x} cy={point1.y} r="3" fill="red" />
+            {showMidpoint && (
+              <circle cx={(point1.x + point2.x) / 2} cy={(point1.y + point2.y) / 2} stroke='black' r="2" fill="pink" />
+            )}
+          </svg>
+          <button
+            className="bg-blue-500 text-white px-4 py-2 rounded ml-4 absolute bottom-3 left-3"
+            onClick={() => setPlay(!play)}
+          >
+            {play ? 'Pause' : 'Play'}
+          </button>
+        </div>
+
+        <div className="flex space-x-3">
+          <div className='flex flex-col justify-evenly h-full'>
+            <div>
+              <input className='m-2 align-middle' type='checkbox' id='isEllipse' checked={showEllipse} onChange={() => setShowEllipse(!showEllipse)} />
+              <label className='align-middle select-none' htmlFor='isEllipse'>Use Ellipse</label>
+            </div>
+            <div>
+              <input className='m-2 align-middle' type='checkbox' id='showNeighbors' checked={showActiveNeighbors} onChange={() => setShowActiveNeighbors(!showActiveNeighbors)} />
+              <label className='align-middle select-none' htmlFor='showNeighbors'>Show Active Neighbors (grey)</label>
+            </div>
+            <div>
+              <input className='m-2 align-middle' type='checkbox' id='showNextPred' checked={showNextPredictedPoint} onChange={() => setShowNextPredictedPoint(!showNextPredictedPoint)} />
+              <label className='align-middle select-none' htmlFor='showNextPred'>Show Next Predicted Point (orange)</label>
+            </div>
+            <div>
+              <input className='m-2 align-middle' type='checkbox' id='showMidpoint' checked={showMidpoint} onChange={() => setShowMidpoint(!showMidpoint)} />
+              <label className='align-middle select-none' htmlFor='showMidpoint'>Show Midpoint (pink)</label>
+            </div>
+            <div>
+              <input className='m-2 align-middle' type='checkbox' id='showTracedMidpointPath' checked={showTracedMidpointPath} onChange={() => setShowTracedMidpointPath(!showTracedMidpointPath)} />
+              <label className='align-middle select-none' htmlFor='showTracedMidpointPath'>Show Traced Mindpoint path (orange)</label>
+            </div>
           </div>
+          <div className="space-y-4 flex-1">
+            {!showEllipse && (
+              <div>
+                <label className="block text-sm font-medium mb-2">Number of Sides: {sides}</label>
+                <Slider
+                  value={[sides]}
+                  min={3}
+                  max={10}
+                  step={1}
+                  onValueChange={(value) => setSides(value[0])}
+                />
+              </div>
+            )}
 
-          <div>
-            <button
-              className="bg-blue-500 text-white px-4 py-2 rounded"
-              onClick={() => setPlay(!play)}
-            >
-              {play ? 'Pause' : 'Play'}
-            </button>
+            {showEllipse && (
+              <div className='flex space-x-2'>
+                <label className="block text-sm font-medium mb-2">Ellipse A: {ellipseA}</label>
+                <Slider
+                  value={[ellipseA]}
+                  min={10}
+                  max={100}
+                  step={1}
+                  onValueChange={(value) => setEllipseA(value[0])}
+                />
 
-            <input className='m-2 ml-4 align-middle' type='checkbox' id='showNeighbors' checked={showActiveNeighbors} onChange={() => setShowActiveNeighbors(!showActiveNeighbors)} />
-            <label className='align-middle select-none' htmlFor='showNeighbors'>Show Active Neighbors (grey)</label>
-            
-            <input className='m-2 ml-4 align-middle' type='checkbox' id='showNextPred' checked={showNextPredictedPoint} onChange={() => setShowNextPredictedPoint(!showNextPredictedPoint)} />
-            <label className='align-middle select-none' htmlFor='showNextPred'>Show Next Predicted Point (orange)</label>
-          </div>
+                <label className="block text-sm font-medium mb-2">Ellipse B: {ellipseB}</label>
+                <Slider
+                  value={[ellipseB]}
+                  min={10}
+                  max={100}
+                  step={1}
+                  onValueChange={(value) => setEllipseB(value[0])}
+                />
+              </div>
+            )}
 
-          <div>
-            <label className="block text-sm font-medium mb-2">Chord Length: {chordLength}%</label>
-            <Slider
-              value={[chordLength]}
-              min={10}
-              max={100}
-              step={1}
-              onValueChange={(value) => setChordLength(value[0])}
-            />
+            <div>
+              <label className="block text-sm font-medium mb-2">Step: {currentPointIndex} / 1000</label>
+              <Slider
+                value={[currentPointIndex]}
+                min={0}
+                max={totalSteps - 1 + 200}
+                step={1}
+                onValueChange={(value) => checkValidAndSetCurrentPointIndex(value[0])}
+              />
+            </div>
+            <div>
+              <label className="block text-sm font-medium mb-2">Chord Length: {chordLength}%</label>
+              <Slider
+                value={[chordLength]}
+                min={10}
+                max={100}
+                step={1}
+                onValueChange={(value) => setChordLength(value[0])}
+              />
+            </div>
           </div>
         </div>
+
       </CardContent>
     </Card>
   );
 };
 
-export default ChordMidpoint;
+export default ChordWalker;
